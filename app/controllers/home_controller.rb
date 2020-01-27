b7daaa6a2e9561f581667a7d9f714af525482f1e
Anastasia Balaban <anastasiabalaban07@gmail.com>
2020-01-28 00:46:57 +0200

    create worker, configure db

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
-    @message = "everything will be fine"
+    @message = "Dynamic"
+
+    @posts = Post.all
+  end
+
+  def increment_async
+    ::IncrementCountWorker.perform_async(params[:post_id])
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: "home#index"
+
+  post "/increment_async", to: "home#increment_async"
 end
